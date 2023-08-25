e8923c4f6138e4f16d9a5ed975e244fe9e95b89d
Prajwal Mahajan <prajwal.mahajan101@gmail.com>
2023-08-25 20:53:07 +0530

    Button Component Added

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -3,8 +3,11 @@ import {
 	createAuthUserWithEmailAndPassword,
 	createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
-import FormInput from '../form-input/form-input.component';
 
+import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
+
+import './sign-up-form.styles.scss';
 const SignUpFrom = () => {
 	const defaultFromFields = {
 		displayName: '',
@@ -52,8 +55,9 @@ const SignUpFrom = () => {
 	};
 
 	return (
-		<div>
-			<h1>Sign up with your email and password</h1>
+		<div className="sign-up-container">
+			<h2>Don't have an account</h2>
+			<span>Sign up with your email and password</span>
 			<form onSubmit={handleSubmit}>
 				<FormInput
 					label="Display Name"
@@ -87,7 +91,7 @@ const SignUpFrom = () => {
 					value={confirmPassword}
 					required
 				/>
-				<button type="submit">Sign Up</button>
+				<Button type="submit">Sign Up</Button>
 			</form>
 		</div>
 	);
