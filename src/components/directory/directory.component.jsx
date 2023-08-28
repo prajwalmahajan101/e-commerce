528c0672be78948eb76e3f7eaa1a94e868ad1826
Prajwal Mahajan <prajwal.mahajan101@gmail.com>
2023-08-28 12:07:02 +0530

    Css confict removed

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import CategoryItem from '../category-item/category-item.component';
+import DirectoryItem from '../directory-item/directory-item.component';
 import './directory.styles.scss';
 
 const Directory = ({ categories }) => {
 	return (
 		<div className="directory-container">
 			{categories.map(({ title, id, imageUrl }) => (
-				<CategoryItem key={id} title={title} imageUrl={imageUrl} />
+				<DirectoryItem key={id} title={title} imageUrl={imageUrl} />
 			))}
 		</div>
 	);
