fa4291779c696e4a82829603d96e3cb194526983
Prajwal Mahajan <prajwal.mahajan101@gmail.com>
2023-08-29 00:38:50 +0530

    redux persist added

--- src/store/store.js
+++ src/store/store.js
@@ -4,9 +4,31 @@ import {
 	applyMiddleware,
 } from 'redux';
 
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
+
+const middleware = [logger];
+const composedEnhancers = compose(applyMiddleware(...middleware));
+
+const persistConfig = {
+	key: 'root',
+	storage,
+	blacklist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+	persistedReducer,
+	undefined,
+	composedEnhancers
+);
+
+export const persistor = persistStore(store);
 
 // const loggerMiddleware = (store) => (next) => (action) => {
 // 	if (!action.type) {
@@ -19,7 +41,3 @@ import { rootReducer } from './root-reducer';
 // 	next(action);
 // 	// console.log('next state :', store.getState());
 // };
-
-const middleware = [logger];
-const composedEnhancers = compose(applyMiddleware(...middleware));
-export const store = createStore(rootReducer, undefined, composedEnhancers);
