36d7a1ec79ee616f21ffde345b990d1a31f8b307
Prajwal Mahajan <prajwal.mahajan101@gmail.com>
2023-08-28 13:29:01 +0530

    Some More Styled components Added

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,7 +5,11 @@ import { CartContext } from '../../contexts/cart.context';
 import CartItem from '../cart-item/cart-item.component';
 import Button from '../button/button.component';
 
-import './cart-dropdown.styles.scss';
+import {
+	CartItems,
+	CartDropdownContainer,
+	EmptyMessage,
+} from './cart-dropdown.styles.jsx';
 
 const CartDropdown = () => {
 	const { cartItems } = useContext(CartContext);
@@ -15,14 +19,18 @@ const CartDropdown = () => {
 		navigate('/checkout');
 	};
 	return (
-		<div className="cart-dropdown-container">
-			<div className="cart-items">
-				{cartItems.map((item) => (
-					<CartItem key={item.id} cartItem={item} />
-				))}
-			</div>
+		<CartDropdownContainer>
+			<CartItems>
+				{cartItems.length ? (
+					cartItems.map((item) => (
+						<CartItem key={item.id} cartItem={item} />
+					))
+				) : (
+					<EmptyMessage>Cart is Empty</EmptyMessage>
+				)}
+			</CartItems>
 			<Button onClick={gotoCheckout}>GO TO CHECKOUT</Button>
-		</div>
+		</CartDropdownContainer>
 	);
 };
 
--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,18 +1,18 @@
-import './directory-item.styles.scss';
+import React from 'react';
+import {
+	DirectoryItemContainer,
+	BackgroundImage,
+	Body,
+} from './directory-item.styles.jsx';
 const DirectoryItem = ({ imageUrl, title }) => {
 	return (
-		<div className="directory-item-container">
-			<div
-				className="background-image"
-				style={{
-					backgroundImage: `url(${imageUrl})`,
-				}}
-			/>
-			<div className="directory-item-body">
+		<DirectoryItemContainer>
+			<BackgroundImage imageUrl={imageUrl} />
+			<Body>
 				<h2>{title}</h2>
 				<p>Shop Now</p>
-			</div>
-		</div>
+			</Body>
+		</DirectoryItemContainer>
 	);
 };
 
