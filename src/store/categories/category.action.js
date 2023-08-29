43b84202a0a4ce227cf1bdd4eb3428f97cac4ade
Prajwal Mahajan <prajwal.mahajan101@gmail.com>
2023-08-29 09:31:30 +0530

    Categories with redux thunk

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,5 +1,23 @@
 import CATEGORIES_ACTION_TYPES from './category.types';
 import { createAction } from '../../utils/reducer/reducer.utils';
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 
-export const setCategories = (categoriesMap) =>
-	createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesMap);
+export const fetchCategoriesStart = () =>
+	createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (categories) =>
+	createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categories);
+
+export const fetchCategoriesFailed = (error) =>
+	createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+	dispatch(fetchCategoriesStart());
+	try {
+		const categories = await getCategoriesAndDocuments('categories');
+		console.log(categories);
+		dispatch(fetchCategoriesSuccess(categories));
+	} catch (err) {
+		dispatch(fetchCategoriesFailed(err));
+	}
+};
